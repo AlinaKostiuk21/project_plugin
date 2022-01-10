fc297f0ce165282c57640cff406f426c8882cddc
Alina Kostiuk <alina.hariaha@gmail.com>
2022-01-10 20:05:32 +0200

    Add check attribute valueю.

--- src/plugin.js
+++ src/plugin.js
@@ -1,13 +1,9 @@
-
-// console.log(document.currentScript.getAttribute('isRunning'));
 let getIsRunning = document.currentScript.getAttribute('isRunning');
 
 function plugin (getIsRunning) {
     if (!getIsRunning) {
         return;
     }
-
-
 }
 
 
